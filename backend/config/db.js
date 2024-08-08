afe8cd09261be90b8b16870276749de3086cf99f
chetan mallah <121126522+chetanmallah@users.noreply.github.com>
2024-08-09 01:40:16 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,4 @@
-// creating logic to connect with database
-/*
 
-import mongoose from "mongoose";
-
-export const connectDB = async()=> {
-    await mongoose.connect('mongodb+srv://chetanmallah28:A3867700@cluster0.29bji4n.mongodb.net/Food-Delivery').then(()=>console.log("DB Connected"));
-}
-    */
-
-// env file
 
 import mongoose from "mongoose";
 import dotenv from "dotenv";
