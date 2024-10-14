fd7c138f3acf455e376454082fd13d85118ed7ee
chetan mallah <121126522+chetanmallah@users.noreply.github.com>
2024-10-14 20:10:36 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -48,7 +48,7 @@ const FoodDisplay = ({ category }) => {
     <div className='food-display' id='food-display'>
       <h2>Top dishes near you</h2>
       <div className="food-display-list">
-        {food_list.slice(0, 6).map((item) => {
+        {food_list.slice(0, 8).map((item) => {
           if (category === "All" || category === item.category) { 
             return (
               <FoodItem
